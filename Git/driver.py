03cdf0fefecddef5307d23777a058790e3cff0ba
ConsFontaniliLuca <cons.fontanililuca@sia.eu>
2018-07-09 23:41:49 +0200

    aggiunto check argomenti

--- Git/driver.py
+++ Git/driver.py
@@ -1,6 +1,11 @@
 import sys
 
+def check_arguments(arguments):
+	if len(arguments) < 2:
+		raise Exception('utilizzo: {} nome'.format(arguments[0]))
+
 def main(arguments):
+	check_arguments(arguments)
 	print('Hello {}'.format(arguments[1]))
 
 if __name__ == '__main__':
